394c69270722bba4f9c7683ba2731085b2ea04cf
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-11-09 15:50:49 +0900

    5

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 void main(void) {
-char str[30] = "happy C programming";
-printf("문자열 \" %s \" 의 길이 : %i", str,strlen(str));
-return 0;
+	FILE* fp;
+	fp=fopen("sample.txt","w");
+	char str[100];
+	int i;
+	for(i=0;i<3;i++)
+	{
+	printf("input a word");
+	scanf("%s",str); //포인터라서 &가 필요없음 
+	fprintf(fp,"%s\n",str);
+
+}
+	
+	fclose(fp);
+
+	return 0;
 }
 
